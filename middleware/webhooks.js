12d22acc9b8f2d38fcc48fb9332ab6cfc81c0fed
gregorybesson <gregory.besson@adfab.fr>
2017-11-12 19:50:12 -0800

    fix: There was a bug with the secret variable, + the HMAC calculation was buggy. Thanks to https://github.com/jonpulice/node-shopify-auth/blob/master/lib/main.js, his fix on verifyWebhookHMAC did the trick

--- middleware/webhooks.js
+++ middleware/webhooks.js
@@ -7,9 +7,16 @@ module.exports = function createWithWebhook({ secret, shopStore }) {
     const topic = request.get('X-Shopify-Topic');
     const shopDomain = request.get('X-Shopify-Shop-Domain');
 
+    // Shopify seems to be escaping forward slashes when the build the HMAC
+    // so we need to do the same otherwise it will fail validation
+    // Shopify also seems to replace '&' with \u0026 ...
+    let message = JSON.stringify(data)
+    message = message.split('/').join('\\/');
+    message = message.split('&').join('\\u0026');
+
     const generated_hash = crypto
-      .createHmac('sha256', SHOPIFY_APP_SECRET)
-      .update(JSON.stringify(data))
+      .createHmac('sha256', secret)
+      .update(message)
       .digest('base64');
 
     if (generated_hash !== hmac) {
