aa2bacdb0b1e7a5c59d8bcc6a5cce0544a9e1ff8
Kevin Hughes <kevinhughes27@gmail.com>
2017-11-24 16:20:41 -0500

    don't expect body parser to be used

--- routes/shopifyApiProxy.js
+++ routes/shopifyApiProxy.js
@@ -3,7 +3,7 @@ const { URL } = require('url');
 const ALLOWED_URLS = ['/products', '/orders'];
 
 module.exports = function shopifyApiProxy(request, response, next) {
-  const { query, method, path, body, session } = request;
+  const { query, method, path, session } = request;
   const { shop, accessToken } = session;
 
   const strippedPath = path.split('?')[0].split('.json')[0];
@@ -18,7 +18,7 @@ module.exports = function shopifyApiProxy(request, response, next) {
 
   const fetchOptions = {
     method,
-    body,
+    body: request.read(),
     headers: {
       'Content-Type': 'application/json',
       'X-Shopify-Access-Token': accessToken,
