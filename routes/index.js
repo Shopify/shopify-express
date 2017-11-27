9e4d333bd09a491e5bc723cbf1210fa5f541f2bd
Kevin Hughes <kevinhughes27@gmail.com>
2017-11-27 13:55:44 -0500

    configure bodyParser.raw for the api proxy

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const createWithShop = require('../middleware/withShop');
 const createShopifyAuthRouter = require('./shopifyAuth');
@@ -6,9 +7,10 @@ const shopifyApiProxy = require('./shopifyApiProxy');
 
 module.exports = function createRouter(shopifyConfig) {
   const router = express.Router();
+  const rawParser = bodyParser.raw({type: '*/*'});
 
   router.use('/auth/shopify', createShopifyAuthRouter(shopifyConfig));
-  router.use('/api', createWithShop({ redirect: false }), shopifyApiProxy);
+  router.use('/api', rawParser, createWithShop({ redirect: false }), shopifyApiProxy);
 
   return router;
 };
--- routes/shopifyApiProxy.js
+++ routes/shopifyApiProxy.js
@@ -3,22 +3,16 @@ const { URL } = require('url');
 const ALLOWED_URLS = ['/products', '/orders'];
 
 module.exports = function shopifyApiProxy(request, response, next) {
-  const { query, method, path, session } = request;
+  const { query, method, path, body, session } = request;
   const { shop, accessToken } = session;
 
-  const strippedPath = path.split('?')[0].split('.json')[0];
-
-  const inAllowed = ALLOWED_URLS.some(resource => {
-    return strippedPath === resource;
-  });
-
-  if (!inAllowed) {
+  if (!validRequest(path)) {
     return response.status(403).send('Endpoint not in whitelist');
   }
 
   const fetchOptions = {
     method,
-    body: request.read(),
+    body,
     headers: {
       'Content-Type': 'application/json',
       'X-Shopify-Access-Token': accessToken,
@@ -35,6 +29,14 @@ module.exports = function shopifyApiProxy(request, response, next) {
     })
     .catch(err => response.err(err));
 };
+
+function validRequest(path) {
+  const strippedPath = path.split('?')[0].split('.json')[0];
+
+  return ALLOWED_URLS.some(resource => {
+    return strippedPath === resource;
+  });
+}
 
 function fetchWithParams(url, fetchOpts, query) {
   const parsedUrl = new URL(url);
