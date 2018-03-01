93963abdc71ea6c604decea88afa2d5589d1999a
Mallory Allen <mallory.allen@shopify.com>
2018-03-01 11:34:51 -0500

    ➕✅ add prop-types for validating config objects

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+const PropTypes = require('prop-types');
 const createRouter = require('./routes');
 const createMiddleware = require('./middleware');
 const {MemoryStrategy} = require('./strategies');
 
+const ShopifyConfigTypes = {
+  apiKey: PropTypes.string.isRequired,
+  host: PropTypes.string.isRequired,
+  secret: PropTypes.string.isRequired,
+  scope: PropTypes.arrayOf(PropTypes.string).isRequired,
+  afterAuth: PropTypes.func.isRequired,
+  shopStore: PropTypes.Object,
+};
+
 module.exports = function shopify(shopifyConfig) {
+  PropTypes.checkPropTypes(ShopifyConfigTypes, shopifyConfig, 'option', 'ShopifyExpress');
+
   const config = Object.assign(
     {shopStore: new MemoryStrategy()},
     shopifyConfig,
