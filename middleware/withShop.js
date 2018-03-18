ba5dc32b9714bc7057dd29c2a73dc80a59559760
hpawe01 <friedrich.pawelka@googlemail.com>
2018-03-18 01:08:36 +0100

    Follow shopify coding style

--- middleware/withShop.js
+++ middleware/withShop.js
@@ -1,26 +1,27 @@
 module.exports = function withShop({ authBaseUrl } = {}) {
   return function verifyRequest(request, response, next) {
-    const { session, baseUrl } = request;
-    let shop = request.query.shop;
+    const { query = {}, session = {}, baseUrl } = request;
+    const { accessToken } = session;
+    const shop = getShopFromReferrer(request.get('referer')) || query.shop;
 
-    if (!shop && request.get('referer')) {
-      const result = request.get('referer').match(/shop=([^&]+)/);
-      if (result) {
-        shop = result[1];
-      }
-    }
-
-    if (session && session.accessToken && session.shop && session.shop === shop) {
+    if (accessToken && session.shop === shop) {
       next();
       return;
     }
 
     if (shop) {
-      response.redirect(`${authBaseUrl || baseUrl}/auth?shop=${shop}`);
+      response.redirect(`${authBaseUrl || baseUrl}/auth/shopify?shop=${shop}`);
       return;
     }
 
     response.redirect('/install');
-    return;
   };
+
+  function getShopFromReferrer(referrer) {
+    if (!referrer) {
+      return;
+    }
+    const result = referrer.match(/shop=([^&]+)/);
+    return result && result[1];
+  }
 };
