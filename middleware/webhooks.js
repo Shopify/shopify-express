766a3917c797059560f471404a724b75c16f1deb
Gil Greenberg <gilgreenberg@gmail.com>
2018-07-14 13:36:49 -0400

    Fix HMAC failure on webhook post

--- middleware/webhooks.js
+++ middleware/webhooks.js
@@ -13,7 +13,7 @@ module.exports = function configureWithWebhook({ secret, shopStore }) {
         const rawBody = await getRawBody(request);
         const generated_hash = crypto
           .createHmac('sha256', secret)
-          .update(rawBody)
+          .update(rawBody.body)
           .digest('base64');
 
         if (generated_hash !== hmac) {
